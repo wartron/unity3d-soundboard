b467415b338834cdeff6cf5fc54313b0cbe31f61
Will Wharton <willmwharton@gmail.com>
2016-01-30 13:11:17 -0800

    soundboard

---
+++ Assets/Scripts/ButtonPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonPlayer : MonoBehaviour {
+
+    public AudioClip clip;
+
+    public string label;
+
+
+    void Awake()
+    {
+        Text txt = GetComponentInChildren<Text>();
+
+        txt.text = label;
+    }
+
+
+	public void Play ()
+    {
+        Debug.Log("Another 1");
+
+        Soundboard.instance.Play(clip);
+	}
+}
---
+++ Assets/Scripts/Soundboard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soundboard : MonoBehaviour {
+
+
+    public static Soundboard instance;
+
+    private AudioSource _audio;
+
+	void Start () {
+        instance = this;
+	}
+
+    void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+	
+	public void Play (AudioClip clip) {
+        _audio.clip = clip;
+        _audio.Play();
+	}
+}
